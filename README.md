5476f6e5c13dde4929b1bc945bc5ad6617f5eb7b
Rolka <arrk@gft.com>
2021-03-03 16:40:00 +0100

    cleand code, update readme

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 Memory game - you have to find two the same pictures. Each game generates different pictures.
 
 Technologies: HTML5,CSS3, JavaScript
+
+![](memoryGame.gif)
+
+Can be implemented (additional functionalities):
+
+1. Zabezpieczenie kodu aby nie było możliwe wielokrotnie naciśnięcie przycisku "Start".
+2. Zabezpieczenie aby nie można było wybrać innej karty w czasie 1 sek, podczas odliczania timer-a (setTimeout).
+3. Zliczanie punktów (ruchów).
+4. Możliwość zmiany wielkości tablicy do gry.
+5. Odpowiednia wiadomość po odgadnięciu wszystkich kart.
--- main.js
+++ main.js
@@ -1,59 +1,50 @@
-var startButton = document.querySelector('.start_button'); // wybieram elment HTML po klasie
-var gameContainer = document.querySelector('.game_container');
-var ITEMS_COUNT = 16; // Liczba elementow gry = wielkosc tablicy
-var imagesIds = []; // Tablica przetrzumujaca wylosowane ID wykorzystane w grze
-var prevSelectedElem; // Poprzednio wybrany element DOM
-var selectedImagesIds = []; // Tabliac wybranych elementow
+const startButton = document.querySelector('.start_button');
+const gameContainer = document.getElementById('gameContainer')
+const ITEMS_COUNT = 16;
+let imagesIds = [];
+let prevSelectedElem;
+let selectedImagesIds = [];
 
 function onClickCard(event) {
-    var imageId = event.target.dataset.image; // pobierz id wybranego obrazka
-    event.target.classList.remove('unresolved'); // usun classe unresolved
+    let imageId = event.target.dataset.image;
+    event.target.classList.remove('unresolved');
     
-    //jezeli jest to pierwszy wybrany obrazek
     if (selectedImagesIds.length === 0) {
-        prevSelectedElem = event.target; // pobierz klikniety element i zapisujemy go
-        return selectedImagesIds.push(imageId); // dodaj id obrazka do listy obrazow wybranych(do naszej tablicy wybranoch elementow)
+        prevSelectedElem = event.target;
+        return selectedImagesIds.push(imageId);
     } else if (selectedImagesIds[0] && selectedImagesIds[0] === imageId) {
-        // Jezeli wczesniej byl wybrany juz obraz ( selectedImagesIds[0] )
-        // I id obrazka poprzedniego i aktualnie wybranego są rowne
-
-        setTimeout(function() { alert("Match"); }, 2000);//wyswietl komunikat o poprawnym wyborze 
+        setTimeout(function() { alert("Match!"); }, 2000);
 
         setTimeout(function () {
-            event.target.classList.add('resolved'); // dodaj klase do aktualnie wybranego
-            prevSelectedElem.classList.add('resolved'); // dodaj klase do poprzednio wybranego
+            event.target.classList.add('resolved');
+            prevSelectedElem.classList.add('resolved');
         }, 1000)
     } else if (selectedImagesIds[0] && selectedImagesIds[0] !== imageId) {
-        // Jezeli wczesniej byl wybrany juz obraz ( selectedImagesIds[0] )
-        // I id obrazka poprzedniego i aktualnie wybranego są rozne
-        selectedImagesIds = []; // wyzeruj liste poprzednio wybranych obrazkow
+        selectedImagesIds = [];
         setTimeout(function () {
-            event.target.classList.add('unresolved'); // dodaj klase do aktualnie wybranego
-            prevSelectedElem.classList.add('unresolved');// dodaj klase do poprzednio wybranego
+            event.target.classList.add('unresolved');
+            prevSelectedElem.classList.add('unresolved');
         }, 1000)
     }
 }
 
 /**
- * Funkcja generuje jedna karte
- * wraz z klasami
- * oraz event listenerem
+ * Funkcja generuje jedna karte wraz z klasami oraz event listenerem
  * @param {Integer} index 
  */
 function addCard(index) {
-    var containerCard = document.createElement('div'); // utworz kontener dla karty
-    var image = document.createElement('img'); // utworz element DOM dla obrazka
-    containerCard.classList.add('card'); // dodaj odpowiednie klasy
-    containerCard.classList.add('unresolved'); // od poczatku karta jest w stanie 'unresolved'
+    var containerCard = document.createElement('div');
+    var image = document.createElement('img');
+    containerCard.classList.add('card');
+    containerCard.classList.add('unresolved');
 
-    // dodajemy obrazek z wylosowanym id
-    image.src = 'https://unsplash.it/150/150?image=' + imagesIds[index]; 
+    image.src = 'https://unsplash.it/150/150?image=' + imagesIds[index];
 
-    containerCard.dataset.image = imagesIds[index]; // dodaj id do data set
-    containerCard.appendChild(image); // dodaj DOM img do kontenera
-    gameContainer.appendChild(containerCard); // dodaj kontener do pola gry
+    containerCard.dataset.image = imagesIds[index];
+    containerCard.appendChild(image);
+    gameContainer.appendChild(containerCard);
 
-    containerCard.addEventListener('click', onClickCard);  // dodaj listener na klikniecie w karte ????
+    containerCard.addEventListener('click', onClickCard)
 }
 
 /**
@@ -66,18 +57,15 @@ function createCards() {
 }
 
 /**
- * Losowanie wartosci Id dla obrazka
- * w przedziale od 1 do 500
+ * Losowanie wartosci Id dla obrazka w przedziale od 1 do 500
  */
 function randomImageId() {
-    // Math.random() - zwraca wartosc od 0...1
-    // Math.floor() - zaokragla wartosc w dol do najblizszej liczby calkowitej
     return Math.floor(1 + (Math.random() * 1000) % 499);
 }
 
 /**
  * Funkcja mieszajaca kolejnoscia elementy w tablicy
- * @param {Array} a 
+ * @param {Array} a
  */
 function shuffle(a) {
     var j, x, i;
@@ -90,41 +78,25 @@ function shuffle(a) {
 }
 
 function duplicateArray(arr) {
-    return arr.concat(arr.slice()); // polacz tablice (concat) arr z kopia samej siebie (slice)
+    return arr.concat(arr.slice());
 }
 
 function randomImages() {
-    /**
-     * Losujemy ID dla 8 obrazków (ITEMS_COUNT / 2)
-     */    
     for (var i = 0; i < ITEMS_COUNT / 2; i++) {
         var id;
         
         do {
-            id = randomImageId(); // losuj ID dla jednego obrazu
-        } while (imagesIds.indexOf(id) >= 0); // sprawdz czy wczesniej nie zostal wylosowany ????
+            id = randomImageId();
+        } while (imagesIds.indexOf(id) >= 0)
 
-        imagesIds.push(id); // dodaj wylosowany id do listy
+        imagesIds.push(id);
     }
 
-    imagesIds = duplicateArray(imagesIds); // powielamy liste
-    shuffle(imagesIds); // mieszamy kolejnoscia elementy w zduplikowanej liscie
+    imagesIds = duplicateArray(imagesIds);
+    shuffle(imagesIds);
 }
 
-/**
- * Start button on click handler
- */
 startButton.addEventListener('click', function () {
-    randomImages(); // Wylosuj obrazki do gry
-    createCards(); // generuj karty
+    randomImages();
+    createCards();
 })
-
-
-/*DODAC 
-1. Zabezpieczenie kodu aby nie było możliwe wielokrotnie naciśnięcie przycisku "Start".
-2. Zabezpieczenie aby nie można było wybrać innej karty w czasie 1 sek, podczas odliczania timer-a 
-(setTimeout).
-3. Zliczanie punktów (ruchów).
-4. Możliwość zmiany wielkości tablicy do gry.
-5. Odpowiednia wiadomość po odgadnięciu wszystkich kart.
-*/
